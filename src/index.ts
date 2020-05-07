f3057da69ecc2f3c5a5732ff7f0b18ba99f3b97d
Lachlan van der Velden <ldvandervelden@swin.edu.au>
2020-05-07 15:27:08 +1000

    Inital Commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+let myButton = document.getElementById("myButton") as HTMLButtonElement | null;
+if (myButton == null) {
+    alert("Button not found.");
+} else {
+    
+    myButton.onclick = function () {
+        if(myButton == undefined) return;
+        alert(myButton.value);
+    }
+}
